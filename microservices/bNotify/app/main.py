a73af9faf6df6e2003f912df0135b7b120a3d31b
Krzysztof Klodnicki <krzysztof@klodnicki.eu>
2019-12-01 20:26:52 +0100

    bNotify added integration with pubsub

--- microservices/bNotify/app/main.py
+++ microservices/bNotify/app/main.py
@@ -2,10 +2,15 @@ from flask import Flask, request
 from pymessenger.bot import Bot
 from app import app
 import logging
+import base64
+import json
 
 
 access_token = app.config['ACCESS_TOKEN']
-verify_token = app.config['VERIFY_TOKEN'] 
+verify_token = app.config['VERIFY_TOKEN']
+token = app.config['TOKEN']
+
+recipient_id = '2866397626725399'
 
 
 bot = Bot(access_token)
@@ -54,12 +59,40 @@ def verify_fb_token(token_sent):
 def send_message(recipient_id, message_type, response_sent_text, extra_parameter):
     #sends user the text message provided via input response parameter
 
+    logging.warning("Recipient: {} Message: {}".format(recipient_id,response_sent_text))
     if message_type == "text": response = bot.send_text_message(recipient_id, response_sent_text)
     if message_type == "button": response = bot.send_button_message(recipient_id, response_sent_text, extra_parameter)
 
-    logging.info(response)
+    logging.warning("Response : {}".format(response))
 
     return "success"
+
+
+
+
+
+@app.route('/send', methods=['POST'])
+def pubsub_push():
+    if (request.args.get('token', '') !=token):
+        return 'Invalid request', 400
+        
+    envelope = json.loads(request.data.decode('utf-8'))
+    payload = base64.b64decode(envelope['message']['data'])
+    _payload = payload.decode('utf-8')
+
+    logging.warning ("Recieved message from pubsub: {}".format(_payload))
+
+    # send message
+
+    response_type = 'text'
+    response_extra_parameter = 'none'
+    response_sent_text = _payload
+
+    send_message(recipient_id, response_type, response_sent_text, response_extra_parameter)
+    
+    return 'OK', 200
+
+
 
 
 if __name__ == "__main__":
--- microservices/bNotify/config.py
+++ microservices/bNotify/config.py
@@ -7,5 +7,6 @@ class Config(object):
     PROJECT_ID = os.environ.get('PROJECT_ID')
     ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
     VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
+    TOKEN = os.environ.get('TOKEN')
 
 
