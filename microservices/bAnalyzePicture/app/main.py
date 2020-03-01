3510385a87c9aa1af6d8fd1f1db6b591a6ab725a
Krzysztof Klodnicki <krzysztof@klodnicki.eu>
2020-03-01 12:41:31 +0100

    Added stackdriver native api

--- microservices/bAnalyzePicture/app/main.py
+++ microservices/bAnalyzePicture/app/main.py
@@ -9,11 +9,11 @@ from google.cloud import storage
 from google.cloud import pubsub_v1
 from google.cloud import vision
 from google.cloud.vision import types
-import google.cloud.logging
+from google.cloud import logging
 
-client = google.cloud.logging.Client()
-client.setup_logging()
-import logging
+# Stackdriver logging configuration
+client = logging.Client()
+logger = client.logger('banalyzepicture')
 
 project_id = app.config['PROJECT_ID']
 storage_bucket_name = app.config['STORAGE_BUCKET_NAME']
@@ -27,15 +27,20 @@ detection_score = float(app.config['DETECTION_SCORE'])
 
 ## send configuration to logs
 
-logging.warning("vAnalyzePicture starting ... variable values")
-logging.warning("- PROJECT_ID: {}".format(project_id))
-logging.warning("- STORAGE_BUCKET_NAME: {}".format(storage_bucket_name))
-logging.warning("- TOPIC_NAME: {}".format(topic_name))
-logging.warning("- TOPIC_NAME_OUTPUT: {}".format(topic_name_output))
-logging.warning("- TOKEN: {}".format(token))
-logging.warning("- UPLOAD_FOLDER: {}".format(upload_folder))
-logging.warning("- LABEL_TO_DETECT: {}".format(label_to_detect))
-logging.warning("- DETECTION_SCORE: {}".format(detection_score))
+print ("Sending configuration to Stackdriver")
+logger.log_struct(
+		{"message": "banalyzepicture microservice started", 
+		"PROJECT_ID": project_id,
+		"STORAGE_BUCKET_NAME": storage_bucket_name,
+		"TOPIC_NAME": topic_name,
+		"TOPIC_NAME_OUTPUT": upload_folder,
+		"TOKEN": token,
+		"UPLOAD_FOLDER": upload_folder,
+		"LABEL_TO_DETECT": label_to_detect,
+		"DETECTION_SCORE": detection_score,
+		"Hostname": os.uname()[1],
+		"OS version": os.uname()[3]
+		})
 
 MESSAGES = []
 
@@ -52,13 +57,22 @@ def download_file(storage_bucket_name, file_path):
 	blob.download_to_filename(upload_folder + '/' + file_name)
 	return (upload_folder + '/' + file_name)
 
-def send_message_to_pubsub(message):
+def send_message_to_pubsub(message, file_name):
 	publisher = pubsub_v1.PublisherClient()
 
-	logging.warning ("Sending message {} to topic {}".format(message, topic_name_output))
+	print ("Sending message {} to topic {}".format(message, topic_name_output))
+
+	logger.log_struct(
+		{"message": "Sending PubSub message ...", 
+		"PubSub message": message,
+		"PubSub topic": topic_name_output,
+		"Image name": file_name,
+		"Topic path": topic_path
+		})
+
 	message = message.encode('utf-8')
 	topic_path = publisher.topic_path(project_id, topic_name_output)
-	status = publisher.publish(topic_path, message, object=label_to_detect)
+	status = publisher.publish(topic_path, message, object=label_to_detect, file_name=file_name)
 
 
 def analyze_picture(file_name):
@@ -73,7 +87,7 @@ def analyze_picture(file_name):
 	# Performs label detection on the image file
 	response = client.label_detection(image=image)
 	labels = response.label_annotations
-	logging.warning('Results of Vision AI check. Search label: {}'.format(label_to_detect))
+	print('Results of Vision AI check. Search label: {}'.format(label_to_detect))
 	score = 0
 	other_objects = ""
 	for label in labels:
@@ -90,6 +104,7 @@ def analyze_picture(file_name):
 @app.route('/pubsub/push', methods=['POST'])
 def pubsub_push():
 	if (request.args.get('token', '') !=token):
+		logger.log_text('Incorrect token for pubsub call',severity='WARNING')
 		return 'Invalid request', 400
 	
 	envelope = json.loads(request.data.decode('utf-8'))
@@ -98,24 +113,35 @@ def pubsub_push():
 
 	# download file from blob
 
-	logging.warning("Downloading file {} from storage {}".format(payload, storage_bucket_name))
+	print("Downloading file {} from storage {}".format(payload, storage_bucket_name))
 	_payload = payload.decode('utf-8')
 	file_name = download_file(storage_bucket_name, _payload)
 
 	ai_score = analyze_picture(file_name)[0]
 	other_objects = analyze_picture(file_name)[1]
 
+	_log_message =  "VisionAI results - object: {}, check result: {}".format(label_to_detect, round(ai_score,2))
+	print(_log_message)
+	logger.log_struct(
+		{"message": _log_message, 
+		"object to detect": label_to_detect,
+		"detection score": round(ai_score,2),
+		"image name": file_name,
+		"identified objects": other_objects,
+		"send notification about all checks": notify_about_all_checks
+		})
+
 	if ai_score > float(detection_score):
     		message = "Object: {} identified with probability {} on picture {}".format(label_to_detect, round(ai_score,2), file_name)
-    		send_message_to_pubsub(message)
+    		send_message_to_pubsub(message, file_name)
 	else:
 		if notify_about_all_checks == "yes":
 			message = "Object: {} not detected. Other objects are {}. Picture {}".format(label_to_detect, other_objects, file_name)
-			send_message_to_pubsub(message)
+			send_message_to_pubsub(message, file_name)
 
     		
 	# delete local copy of file
-	logging.warning ("Deleting local copy of file {}".format(file_name))
+	print ("Deleting local copy of file {}".format(file_name))
 	os.remove(file_name)
 
 
