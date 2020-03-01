d9074682e3c02335fb19560a90f2572cfc3beb24
Krzysztof Klodnicki <krzyszof@klodnicki.eu>
2020-03-01 18:46:00 +0000

    bUploadPicture logging improvement

--- microservices/bUploadPictureFromCamera/buploadpicture.py
+++ microservices/bUploadPictureFromCamera/buploadpicture.py
@@ -1,18 +1,15 @@
 from dotenv import load_dotenv
 import urllib.request as req
 import imgcompare
-#import logging
 import sys
 import time
 import os
 from datetime import datetime
 from google.cloud import storage
 from google.cloud import pubsub_v1
-from google.cloud import logging
-
-# Stackdriver logging configuration
-client = logging.Client()
-logger = client.logger('buploadpicture')
+import logging
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler
 
 # load env variables in case of local tests
 load_dotenv()
@@ -24,12 +21,29 @@ IMG_COMP_RESULT = float(os.environ.get('IMG_COMP_RESULT'))
 SLEEP_TIME  = int(os.environ.get('SLEEP_TIME_IN_SECONDS'))
 storage_bucket_name = os.environ.get('STORAGE_BUCKET_NAME')
 topic_name = os.environ.get('TOPIC_NAME')
+debug_level = os.environ.get('DEBUG_LEVEL')
+
+# Stackdriver logging configuration
+client = google.cloud.logging.Client()
+handler = CloudLoggingHandler(client,name="buploadpicture")
+cloud_logger = logging.getLogger('cloudLogger')
+
+# debug level configuration
+if (debug_level=="DEBUG"):
+    cloud_logger.setLevel(logging.DEBUG)
+elif (debug_level=="INFO"):
+    cloud_logger.setLevel(logging.INFO)
+else:
+    cloud_logger.setLevel(logging.WARNING)
+
+cloud_logger.addHandler(handler)
 
 
 def publish_file(file_name, img_comp_percentage):
-    logger.log_text('Sending file for AI processing ...')
+
     start_time = time.time()
-    print("<> File sent for AI processing. Difference is {}".format(img_comp_percentage))
+    cloud_logger.debug('Sending file for AI processing with difference score {}'.format(round(img_comp_percentage,2)))
+    print("File sent for AI processing. Difference is {}".format(img_comp_percentage))
 
     # create copy of file with timestamp
     full_path_of_file = os.path.dirname(os.path.abspath(__file__)) + "/" + file_name
@@ -50,15 +64,9 @@ def publish_file(file_name, img_comp_percentage):
     _img_upload_time = round(time.time() - img_upload_start_time,2)
     _pubsub_message_time = round(time.time() - pubsub_start_time,2)
 
-    # send logs to stackdriver
-    logger.log_struct(
-            {"message": "Image sent for AI (overall time {} seconds, detection score {})".format(_overalltime, img_comp_percentage), 
-            "image name": new_name_of_file,
-            "detection score": img_comp_percentage,
-            "time needed to upload file to bucket": _img_upload_time,
-            "time needed to send PubSub message": _pubsub_message_time,
-            "img url in bucket": file_url
-            })
+    cloud_logger.warning("Image sent for AI (overall time {} seconds, detection score {})".format(_overalltime, img_comp_percentage))
+    cloud_logger.info("Image name: {}, detection score: {}".format(new_name_of_file, img_comp_percentage))
+    cloud_logger.debug("Upload file to bucket time: {}, time of sending PubSub: {}".format(_img_upload_time, _pubsub_message_time))
 
 def compare_file():
 
@@ -69,10 +77,11 @@ def compare_file():
     if os.path.isfile('tmp/old_image.jpg'):
         # previous file exists, compare old and new pictures 
         img_comp_percentage = imgcompare.image_diff_percent("tmp/old_image.jpg", "tmp/new_image.jpg")
-        #logging.info("Img compare result: " + str(img_comp_percentage) + " (expected: " + str(IMG_COMP_RESULT) + ")")
+        cloud_logger.debug("Img compare result: " + str(round(img_comp_percentage,3)) + " (expected: " + str(IMG_COMP_RESULT) + ")")
+        print("Img compare result: " + str(round(img_comp_percentage,3)) + " (expected: " + str(IMG_COMP_RESULT) + ")")
     else:
         # previous file does not exist, comparison will be with next run of the job
-        #logging.info("Previous version of image does not exist.")
+        cloud_logger.debug("Previous version of image does not exist.")
         img_comp_percentage = 0
 
     return round(img_comp_percentage,1)
@@ -85,7 +94,7 @@ def upload_file_to_storage(filename):
         blob = bucket.blob(filename)
         blob.upload_from_filename(filename='tmp/'+filename)
         blob_file_url = blob.public_url
-        print("File {} uploaded to storage bucket".format(filename))
+        cloud_logger.debug("File {} uploaded to storage bucket {}".format(filename, storage_bucket_name))
 
         return (blob_file_url)
 
@@ -97,22 +106,18 @@ def send_message_to_pubsub(message):
     topic_path = publisher.topic_path(project_id, topic_name)
     status = publisher.publish(topic_path, message, source='buploadpicture' )
 
-    print("Message sent to Pub/Sub topic {}".format(topic_name))
+    cloud_logger.debug("Message {} sent to Pub/Sub topic {}".format(message, topic_name))
 
 
 def main():
 
     pod_start_time = time.time()
     # logging 
-    print("<<< >>> Starting processing. Frequency of job: every {} seconds, configured image comparison factor is {}".format(SLEEP_TIME, IMG_COMP_RESULT))
-    logger.log_struct(
-            {"message": "bUploadPicture - starting microservice", 
-            "Frequency of job in seconds": SLEEP_TIME, 
-            "image comparison factor is": IMG_COMP_RESULT, 
-            "image source url": img_url, 
-            "destination storage bucket": storage_bucket_name,
-            "pubsub topic name": topic_name
-            } )
+    print("Starting processing. Frequency of job: every {} seconds, configured image comparison factor is {}".format(SLEEP_TIME, IMG_COMP_RESULT))
+    cloud_logger.warning("bUploadPicture - starting microservice. Frequency of job: every {} seconds, configured image comparison factor is {}".format(SLEEP_TIME, IMG_COMP_RESULT))
+    cloud_logger.warning("Debug level: {}, hostname: {}".format(debug_level, os.uname()[1]))
+    cloud_logger.info("Image source url: {}, destination storage bucket: {}, PubSub topic name: {} ".format(img_url, storage_bucket_name, topic_name))
+    
 
     ########### main loop
     counter = 0 # list of processing cycles
@@ -143,7 +148,7 @@ def main():
                 if os.path.isfile(os.path.join(path, filename)):
                     os.remove(os.path.join(path, filename))
                     print (" deleted old file {}".format(filename))
-                    logger.log_text('Deleted old picture: '.format(filename))
+                    cloud_logger.debug('Deleted old picture: '.format(filename))
 
         # statistics
         if counter > statistics_sent_counter + int((round((3600 / SLEEP_TIME),0))) :
@@ -155,15 +160,10 @@ def main():
             else:
                 ave_img_comp_percentage = 0
 
-            logger.log_struct(
-                    {"message": "bUploadPicture - heartbeat - statistics (hours {}) ".format(_podlife), 
-                    "Frequency of job in seconds": SLEEP_TIME, 
-                    "image comparison detector is": IMG_COMP_RESULT,
-                    "pod life in hours": _podlife,
-                    "pictures analyzed": counter,
-                    "pictures sent for AI": sent_for_ai_counter,
-                    "average image difference factor": ave_img_comp_percentage
-                    } )
+            cloud_logger.warning("bUploadPicture - heartbeat - statistics (hours {}) hostname {}".format(_podlife,os.uname()[1]))
+            cloud_logger.warning("Picture analyzed {}, pictures sent for AI {} ".format(counter, sent_for_ai_counter))
+            cloud_logger.warning("Image comparison detector is {}, verage image difference factor {}".format(IMG_COMP_RESULT, ave_img_comp_percentage))
+
             statistics_sent_counter = counter
 
         # wait for next check
